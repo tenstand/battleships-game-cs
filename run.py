1fa0e9e970ef46de3eac77af0599b60468e59d65
tenstand <christensmith_@msn.com>
2022-04-05 12:28:21 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+# Legend:
+EMPTY = 0
+SHIP = 1
+HIT = 2
+MISS = 3
+
+def getBoard(boardSize):
+    board = []
+    for i in range(boardSize):
+        board.append([EMPTY] * boardSize)
+    return board
